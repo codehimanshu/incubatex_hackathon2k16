e42cacaefd729bf8a3ae9a97e9f15e0fa84f07a3
CodeHimanshu <himanshuagrawal1998@gmail.com>
2016-08-06 15:32:46 +0530

    index

--- webapp/check.php
+++ webapp/check.php
@@ -1,5 +1,21 @@
     <?php
       session_start();
       require_once 'db_connect.php';
-      
+      if(isset($_POST["login"]))
+      {
+      	$username=$_POST["name"];
+      	$password=$_POST["password"];
+      	$_SESSION["user"]="logged";
+      }
+      else if(isset($_POST["signup"]))
+      {
+      	$email=$_POST["email"];
+      	$name=$_POST["name"];
+      	$password=$_POST["password"];
+      	$_SESSION["user"]="logged";
+      }
+      else
+      {
+      	$_SESSION["user"]="guest";
+      }
     ?>

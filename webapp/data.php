7943a9ff98408b86739648a3aa4267885256f665
CodeHimanshu <himanshuagrawal1998@gmail.com>
2016-08-06 23:21:54 +0530

    data update

--- webapp/data.php
+++ webapp/data.php
@@ -1,13 +1,5 @@
 <?php
 
-if(isset($_POST['id']))
-{ echo "0";
-  $id=$_POST["id"];
-  $result=mysql_query("SELCET * FROM user WHERE id='$id'",$link);
-  $data=mysql_fetch_all("$result");
-  echo $data["username"];
-}
-
 	if(isset($_POST['id']))
 	{
 		require_once 'db_connect.php';
@@ -17,7 +9,7 @@ if(isset($_POST['id']))
 		while($row=mysql_fetch_assoc($result)):
 		?>
 		<?php
-			if($row["senqder"]==$id):
+			if($row["sender"]==$id):
 			?>
 			<div class=sender>
 				<?php echo $row["message"]; ?>

a8fea387071a3c0f548addaf5bc17ebc83312b05
CodeHimanshu <himanshuagrawal1998@gmail.com>
2016-08-06 22:44:21 +0530

    Chat read

--- webapp/data.php
+++ webapp/data.php
@@ -1,10 +1,28 @@
 <?php
-
-if(isset($_POST['id']))
-{ echo "0";
-  $result=mysql_query("SELCET * FROM user WHERE id='$id'",$link);
-  $data=mysql_fetch_all("$result");
-  echo $data["username"];
-}
-
+	if(isset($_POST['id']))
+	{
+		require_once 'db_connect.php';
+		$id=$_POST["id"];
+		$result=mysql_query("SELECT * FROM message WHERE sender='$id' OR receiver='$id'",$link) or die(mysql_error());
+		if($result)
+		while($row=mysql_fetch_assoc($result)):
+		?>
+		<?php
+			if($row["sender"]==$id):
+			?>
+			<div class=sender>
+				<?php echo $row["message"]; ?>
+				<?php echo $row["time"]; ?>
+			</div>
+			<?php
+			else:
+			?>
+			<div class=receiver>
+				<?php echo $row["message"]; ?>
+				<?php echo $row["time"]; ?>
+			</div>
+			<?php 
+			endif;
+		endwhile;
+	}
 ?>
